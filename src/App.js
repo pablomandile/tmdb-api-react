f171da16797bbcdeb3779a0cf2639d3d807f15ec
pablo.mandile@bue.edu.ar <pablo.mandile@bue.edu.ar>
2022-12-16 19:17:15 -0300

    agrego función de click, trae datos e imagen poster en banner

--- src/App.js
+++ src/App.js
@@ -11,9 +11,9 @@ function App() {
 
   // variables de estado
   const [movies, setMovies] = useState([]);
-  const [searchKey, setSearchKey] = useState();
+  const [searchKey, setSearchKey] = useState("");
   const [trailer, setTrailer] = useState(null);
-  const [movie, setMovie] = useState({ title: "Loading movies..." });
+  const [movie, setMovie] = useState({ title: "Cargando Películas..." });
   const [playing, setPlaying] = useState(false);
 
   //funcion para realizar la petición por get a la api
@@ -27,6 +27,37 @@ function App() {
     });
     setMovies(results);
     setMovie(results[0]);
+
+    if (results.lenght){
+      await traerPelicula(results[0].id)
+    }
+
+  }
+
+
+
+  // traer la info de un objeto y mostrar trailer en reproductor
+  const traerPelicula = async(id) =>{
+    const {data} = await axios.get(`${API_URL}/movie/${id}` , {
+      params: {
+        api_key: API_KEY,
+        append_to_response: "video"
+      }
+    })
+
+    if(data.videos && data.videos.results){
+      const trailer = data.videos.results.find(
+        (vid) => vid.name === "Official Trailer"
+      );
+      setTrailer(trailer ? trailer : data.videos.results[0])
+    }
+    setMovie(data)
+  }
+
+  const seleccionarPelicula = async(movie) => {
+    traerPelicula(movie.id)
+    setMovie(movie)
+    window.scrollTo(0,0)
   }
 
   // funcion para buscar peliculas por nombre
@@ -37,7 +68,7 @@ function App() {
 
   useEffect(() => {
     traerPeliculas();
-  },[]);
+  }, []);
 
   return (
     <div>
@@ -51,11 +82,71 @@ function App() {
           <button className='btn btn-primary ms-3'>Buscar</button>
         </div>
       </form>
+      
+      {/* contenedor del banner y reproductor de video */}
+      <div>
+        <main>
+          {movie ? (
+            <div className='verTrailer' 
+              style={{
+                backgroundImage: `url("${IMAGE_PATH}${movie.backdrop_path}")`,
+              }}
+            >
+              {playing ? (
+                <>
+                  <YouTube
+                    videoId={trailer.key}
+                    classname="reproductor container"
+                    containerClassName={"youtube-container amru"}
+                    opts={{
+                      width: "100%",
+                      height: "100%",
+                      playerVars: {
+                        autoplay: 1,
+                        controls: 0,
+                        cc_load_policy: 0,
+                        fs: 0,
+                        iv_load_policy: 0,
+                        modestbranding: 0,
+                        rel: 0,
+                        showinfo: 0,
+                      }
+                    }}
+                    />
+                    <button onClick={() => setPlaying(false)} className="boton">
+                      Cerrar
+                    </button>
+                </>
+              ) : (
+                <div className='container'>
+                  <div className=''>
+                    {trailer ? (
+                      <button 
+                        className='boton'
+                        onClick={()=> setPlaying(true)}
+                        type="button"  
+                      >
+                        Ver trailer
+                      </button>
+                    ) : (
+                      ""
+                      // "Lo siento, no se encuentra el trailer"
+                    )}
+                    <h1 className='text-white'>{movie.title}</h1>
+                    <p className='text-white'>{movie.overview}</p>
+                  </div>
+                </div>
+              )}
+            </div>
+          ) : null}
+        </main>
+      </div>
+
       {/* contenedor que va a mostrar las peliculas actuales */}
       <div className='container mt-3'>
         <div className='row'>
           {movies.map((movie) => (
-            <div key={movie.id} className='col-md-4 mb-3'>
+            <div key={movie.id} className='col-md-4 mb-3' onClick={()=> seleccionarPelicula(movie)}>
               <img src={`${URL_IMAGE + movie.poster_path}`} alt="" height={600} width="100%" />
               <h4 className='text-center'>{movie.title}</h4>
             </div>
